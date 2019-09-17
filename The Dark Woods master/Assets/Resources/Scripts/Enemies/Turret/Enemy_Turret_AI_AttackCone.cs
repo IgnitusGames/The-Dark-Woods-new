121a3e6707565e3dedafa5f3d939589a70a77428
martijnvamst <martijnvamst@hotmail.com>
2019-09-17 17:31:22 +0200

    watiseraandehand

---
+++ The Dark Woods master/Assets/Resources/Scripts/Enemies/Turret/Enemy_Turret_AI_AttackCone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Turret_AI_AttackCone : MonoBehaviour
+{
+
+    public Turret_Ai turretAI;
+    public bool isLeft = false;
+    private void Awake()
+    {
+        turretAI = gameObject.GetComponentInParent<Turret_Ai>();
+    }
+
+
+    void OnTriggerStay2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Player")
+        {
+            
+            if (isLeft)
+            {
+                turretAI.Attack(false);
+                
+            }
+            else
+            {
+                turretAI.Attack(true);
+           
+            }
+        }
+    }
+
+    
+        
+}
+
+
