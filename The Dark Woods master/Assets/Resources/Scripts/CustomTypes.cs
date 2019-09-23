b8af9ee9ac118aedc515a85c920f04c2a4f9f801
Ino van den Berg <0928787@hr.nl>
2019-09-23 11:20:12 +0200

    Jump button fix

--- The Dark Woods master/Assets/Resources/Scripts/CustomTypes.cs
+++ The Dark Woods master/Assets/Resources/Scripts/CustomTypes.cs
@@ -15,10 +15,27 @@ public class SaveData
 {
     string world_level;
     float[] player_poition;
+    int crystal_score;
     int player_level;
+    int player_health;
     int gold_score;
-    public SaveData()
+    bool player_has_reached_checkpoint;
+    public SaveData(GameManager game_state, string level_name)
     {
-
+        this.world_level = level_name;
+        this.player_poition = ConvertPosition(game_state.player_position);
+        this.crystal_score = game_state.crystal_score;
+        this.player_level = game_state.player_level;
+        this.player_health = game_state.player_health;
+        this.gold_score = game_state.gold_score;
+        this.player_has_reached_checkpoint = game_state.player_has_reached_checkpoint;
+    }
+    private float[] ConvertPosition(Vector3 player_position)
+    {
+        float[] converted_position = new float[3];
+        converted_position[0] = player_position.x;
+        converted_position[1] = player_position.y;
+        converted_position[2] = player_position.z;
+        return converted_position;
     }
 }
--- The Dark Woods master/Assets/Resources/Scripts/Environment/SaveGameTrigger.cs
+++ The Dark Woods master/Assets/Resources/Scripts/Environment/SaveGameTrigger.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveGameTrigger : MonoBehaviour
 {
     public bool is_checkpoint = true;
+
+    private string current_level;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            current_level = SceneManager.GetActiveScene().name;
             if(is_checkpoint)
             {
                 //save game met positie
