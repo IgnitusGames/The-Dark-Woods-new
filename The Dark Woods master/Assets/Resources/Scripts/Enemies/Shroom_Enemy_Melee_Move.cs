a482a9152cf1d6dd143998fa1e6e150a36b34927
mdb <0924438@hr.nl>
2019-09-25 14:01:30 +0200

    large commit 4

--- The Dark Woods master/Assets/Resources/Scripts/Enemies/Shroom_Enemy_Melee_Move.cs
+++ The Dark Woods master/Assets/Resources/Scripts/Enemies/Shroom_Enemy_Melee_Move.cs
@@ -16,12 +16,17 @@ public class Shroom_Enemy_Melee_Move : MonoBehaviour
     public float hitInterval;
     public float attacktimer = 20.0f;
     public float hitDistance;
-
+    private PlayerLogic player_logic;
+    
+    private void Start()
+    {
+        player_logic = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLogic>();
+    }
     void Update()
     {
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + raycast_hit_offset, transform.position.y), new Vector2(XMoveDirection, 0));
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
-
+        hitTimer += Time.deltaTime;
         // Debug.Log(hit.distance);
 
         if (hit.distance < 1.0f && hit.collider.tag == "EnemyCollider")
@@ -30,8 +35,13 @@ public class Shroom_Enemy_Melee_Move : MonoBehaviour
 
             //  player_Health.Die();    
         }
-
-        Attack();
+        if (hit.collider.tag != "Player" && hit.distance > hitDistance)
+        {
+            EnemySpeed = 4;
+            animator.SetBool("IsAttack", false);
+            //animator.SetBool("IsAttack", false);
+        }
+        // Attack();
 
         // EnemySpeed = 4;
     }
@@ -48,35 +58,52 @@ public class Shroom_Enemy_Melee_Move : MonoBehaviour
             gameObject.GetComponent<Animation>().Play("shroommeleedmg");
             print("pannenkoekn");
         }
-        
+     
+
     }
 
 
     public void Attack()
     {
-        hitTimer += Time.deltaTime;
+        
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + raycast_hit_offset, transform.position.y), new Vector2(XMoveDirection, 0));
 
         //bulletTimer >= bulletInterval
 
-
-        if (hit.collider.tag == "Player" && hit.distance < hitDistance && hitTimer >= hitInterval)
+        print(hit.distance);
+        print(hit.collider.tag);
+        if(facingRight == true)
         {
-
-            EnemySpeed = 0;
-            hit.collider.gameObject.GetComponent<Player_Health_Collectible>().Damage(1);
-            hitTimer = 0;
-            print("valalalltl aana");
-            FindObjectOfType<AudioManager>().Play("ShroomEnemyAttack");
-            animator.SetBool("IsAttack", true);
+            if (hit.collider.tag == "Player" && hit.distance < hitDistance && hitTimer >= hitInterval)
+            {
+                raycast_hit_offset = (1.5f);
+                EnemySpeed = 0;
+                hit.collider.gameObject.GetComponent<Player_Health_Collectible>().Damage(1);
+                hitTimer = 0;
+                print("valalalltl aana");
+                FindObjectOfType<AudioManager>().Play("ShroomEnemyAttack");
+                animator.SetBool("IsAttack", true);
+                player_logic.StartCoroutine(player_logic.KnockBack(0.02f, 50, Vector2.left*200));
+            }
         }
-        if (hit.collider.tag != "Player" && hit.distance > hitDistance)
+        if (facingRight == false)
         {
-            EnemySpeed = 4;
-            animator.SetBool("IsAttack", false);
-            //animator.SetBool("IsAttack", false);
+            raycast_hit_offset = (-1.5f);
+            if (hit.collider.tag == "Player" && hit.distance < hitDistance && hitTimer >= hitInterval)
+            {
+
+                EnemySpeed = 0;
+                hit.collider.gameObject.GetComponent<Player_Health_Collectible>().Damage(1);
+                hitTimer = 0;
+                print("valalalltl aana");
+                FindObjectOfType<AudioManager>().Play("ShroomEnemyAttack");
+                animator.SetBool("IsAttack", true);
+                player_logic.StartCoroutine(player_logic.KnockBack(0.02f, 50, Vector2.right * 200));
+            }
         }
+
+  
 
 
 
